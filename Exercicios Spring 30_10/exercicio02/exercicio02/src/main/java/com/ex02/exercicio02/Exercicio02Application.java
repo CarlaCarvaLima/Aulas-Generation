ba7396fcdd35746b4994e38a9cefe9061539fb24
CarlaCarvaLima <carlacarvalho93@hotmail.com>
2020-10-30 23:01:06 -0300

    v1.0

---
+++ Exercicios Spring 30_10/exercicio02/exercicio02/src/main/java/com/ex02/exercicio02/Exercicio02Application.java
@@ -0,0 +1,13 @@
+package com.ex02.exercicio02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio02Application.class, args);
+	}
+
+}

d33fb38fa9580ea4d11172f56399e381936f71d8
Carla-Lima <carlacarvalho93@hotmail.com>
2020-11-06 18:32:19 -0300

    v1.1

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	
-	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {	
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo); 
 
 }
